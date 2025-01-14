a528689eecb4cf3e064bfc6b4952dce173bbf9c7
SERENA FERRARO <serenaferraro21@gmail.com>
2025-01-14 16:30:06 +0100

    Es.3

--- src/EsercizioTre.java
+++ src/EsercizioTre.java
@@ -14,11 +14,16 @@ public class EsercizioTre {
         System.out.println(phoneBook);
 
         System.out.println("rimuoviamo un contatto");
-        phoneBook.remove("Luigi");
-        System.out.println(phoneBook);
+         phoneBook.remove("Luigi");
+         System.out.println(phoneBook);
+        System.out.println("Nella tua rubrica ci sono " + phoneBook.size() + "contatti");
+       // System.out.println(phoneBook.containsKey("Martina"));; // dispaly true o false
 
-
-
+        //Stampiamo la rubrica in un formato tabella
+        for(String i : phoneBook.keySet()){
+            System.out.print(i + "\t" + "= ");
+            System.out.println(phoneBook.get(i));
+        }
 
     }
 }
